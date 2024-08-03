e2086b6fc70d26fd03b8065c38e3fbcde5ac01b1
Taku Yamamoto <jts0ymt@outlook.jp>
2024-08-03 22:48:51 +0900

    全画面表示のコードを書いた

--- app/src/main/java/com/mygame/vknovel/MainActivity.kt
+++ app/src/main/java/com/mygame/vknovel/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.mygame.vknovel
 
-import android.view.View
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.google.androidgamesdk.GameActivity
 
 class MainActivity : GameActivity() {
@@ -10,20 +13,14 @@ class MainActivity : GameActivity() {
         }
     }
 
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) {
-            hideSystemUi()
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        hideSystemUi()
     }
 
     private fun hideSystemUi() {
-        val decorView = window.decorView
-        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        val insetsController=WindowCompat.getInsetsController(window,window.decorView)
+        insetsController.hide(WindowInsetsCompat.Type.systemBars())
+        insetsController.systemBarsBehavior= WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
